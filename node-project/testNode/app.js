4890d284aedb2c1cadb5a07b32c5846691f25c0b
vision20400 <vision20400@naver.com>
2022-07-26 17:15:25 +0000

    credotServerRDS

--- node-project/testNode/app.js
+++ node-project/testNode/app.js
@@ -9,6 +9,9 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+const mariadb = require("./database/mariadb.js");
+mariadb.connect;
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
