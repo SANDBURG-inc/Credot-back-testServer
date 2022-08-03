db923eb7d711843eaf7b783984fb9fc6e5c4cf55
seunghun <juniper0917@gmail.com>
2022-08-04 01:21:54 +0900

    SessionCookieApi update

--- node-project/testNode/routes/database/register.js
+++ node-project/testNode/routes/database/register.js
@@ -2,6 +2,18 @@ var database = require("express").Router();
 const url = require("url");
 var mariadb = require("mysql");
 
+const con = mariadb.createConnection({
+  host: "credot-rds.cccnip9rb8nn.ap-northeast-2.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "sandburg123",
+  database: "credotClient",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+});
+
 database.get("/register", function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
