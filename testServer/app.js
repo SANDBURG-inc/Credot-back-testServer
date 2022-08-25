52b4d360a68d480283e21106afd7be7ded7b9c73
seunghun <juniper0917@gmail.com>
2022-08-25 18:04:19 +0900

    fix

--- testServer/app.js
+++ testServer/app.js
@@ -84,8 +84,6 @@ passport.deserializeUser(function (email, done) {
 });
 
 app.get("/login", function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
   passport.authenticate("local", function (err, user, info) {
     if (err) {
       return next(err);
