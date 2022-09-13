b17b5b5259076854cd9bcde55b9ce9f8e04485b5
seunghun <juniper0917@gmail.com>
2022-09-13 20:45:33 +0900

    seunghun cookie

--- testServer/app.js
+++ testServer/app.js
@@ -97,18 +97,19 @@ app.post("/login", function (req, res, next) {
           return next(err);
         }
         console.log(user);
+        res.cookie("seunghuncookie", {
+          secure: true,
+          httpOnly: true,
+          SameSite: "none",
+          maxAge: 600000,
+          domain: ".credot.kr",
+        });
         return res.send(json);
       });
     } else {
       // 로그인 실패
       console.log("/login fail!!!");
-      res.cookie({
-        secure: true,
-        httpOnly: true,
-        SameSite: "none",
-        maxAge: 600000,
-        domain: ".credot.kr",
-      });
+      res.send(false);
     }
   })(req, res, next);
 });
