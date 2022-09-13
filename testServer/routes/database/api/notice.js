9f3083e3e6b9d2594646960cebd47b8d44c30def
mnseong <mskweon@icloud.com>
2022-09-13 21:06:54 +0900

    notice database api

---
+++ testServer/routes/database/api/notice.js
@@ -0,0 +1,16 @@
+var router = require("express").Router();
+const url = require("url");
+
+router.get("/", (req, res) => {
+  var sql = "SELECT title,contents,date FROM notice";
+  console.log("?????");
+  con.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+    return res.send(result);
+  });
+});
+
+module.exports = router;
