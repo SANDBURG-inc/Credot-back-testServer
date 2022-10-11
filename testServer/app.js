e14b7cbd736866aac63ef709d3e3ee86640aa6a3
seunghun <juniper0917@gmail.com>
2022-10-11 21:37:39 +0900

    corpAuthAPI

--- testServer/app.js
+++ testServer/app.js
@@ -12,11 +12,12 @@ const session = require("express-session");
 const corsOptions = require("./option/corsOption");
 const sessionOption = require("./option/sessionOption");
 const fetch = require("node-fetch");
+const url = require("url");
 
 const passportRouter = require("./passport/passport");
 const commerceRouter = require("./routes/commerce/commerceController");
 const dbRouter = require("./routes/database/databaseController");
-const getCorpState = require("./getCorpState/getCorpState");
+const getCorpState = require("./getCorpState/corpAuthentication");
 
 const app = express();
 
@@ -24,7 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 9000);
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(session(sessionOption));
 app.use(cookieParser());
 app.use(logger("dev"));
@@ -49,7 +50,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  getCorpState(res);
+  var queryData = url.parse(req.url, true).query;
+  getCorpState(res, queryData.code);
 });
 
 var server = app.listen(app.get("port"), () => {
