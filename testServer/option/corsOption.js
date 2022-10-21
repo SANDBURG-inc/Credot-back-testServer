b65c3c5f0fe3f679e8b49420c2c1580e1d0dcfbe
seunghun <juniper0917@gmail.com>
2022-10-21 17:18:46 +0900

    whitelist Rollback

--- testServer/option/corsOption.js
+++ testServer/option/corsOption.js
@@ -6,7 +6,13 @@ const whitelist = [
 ];
 
 const corsOptions = {
-  origin: whitelist,
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("NOT allowed"));
+    }
+  },
   credentials: true,
 };
 
