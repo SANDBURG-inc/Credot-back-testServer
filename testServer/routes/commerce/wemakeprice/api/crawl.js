01e66012fcafcea2d4c890f3fd8d8cfedce60742
seunghun <juniper0917@gmail.com>
2022-11-22 18:19:14 +0900

    CorpModal module update

--- testServer/routes/commerce/wemakeprice/api/crawl.js
+++ testServer/routes/commerce/wemakeprice/api/crawl.js
@@ -4,7 +4,9 @@ const modules = require("./modules");
 const crawl = async (req, res) => {
   let queryData = url.parse(req.url, true).query;
   let idpwError = await modules.isIdPwError(queryData, res);
+  let isModal = await modules.isCorpModal();
   console.log("idpwError: ", idpwError);
+  console.log(isModal);
 };
 
 module.exports = crawl;
